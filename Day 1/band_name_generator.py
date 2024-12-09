8609fe1fe05ba92ddda53e79cc28f94e21595c4c
Daniel Feuser da Silva <156859842+DanielFeuser@users.noreply.github.com>
2024-12-08 21:11:35 -0300

    Add files via upload

---
+++ Day 1/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!")
+users_city = input("What's the name os the city you grew up in?\n")
+users_pet = input("What's  your pet's name?\n")
+print("Your band name could be "+users_city+" "+users_pet)
